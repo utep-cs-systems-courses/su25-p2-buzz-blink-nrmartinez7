d47ff48b0b01161e365d9acd2defa047471941d4
“nrmartinez7” <mtnznayeli@gmail.com>
2025-07-27 18:13:16 -0600

    red and green dimming and brightning (opposite directions)

--- demos/4-led-blink-pattern/main.c
+++ demos/4-led-blink-pattern/main.c
@@ -3,6 +3,20 @@
 #include "libTimer.h"
 #include "led.h"
 
+//functions
+void updateGreenBlinking();
+void updateRedBlinking();
+void brightnessPattern();
+
+//global vars
+int greenLim=1;
+int redLim=7;
+int greenCount=0;
+int redCount=0;
+int secondCount=0; //state var representing repeating time 0..1s
+int state=0;
+int speed=1; 
+
 int main(void) {
   P1DIR |= LEDS;
   P1OUT &= ~LED_GREEN;
@@ -14,29 +28,47 @@ int main(void) {
   or_sr(0x18);			/* CPU off, GIE on */
 }
 
-// global state vars that control blinking
-int blinkLimit = 5;  // duty cycle = 1/blinkLimit
-int blinkCount = 0;  // cycles 0...blinkLimit-1
-int secondCount = 0; // state var representing repeating time 0…1s
 
 void
 __interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
 {
-  // handle blinking 
-  blinkCount ++;
-  if (blinkCount >= blinkLimit) { // on for 1 interrupt period
-    blinkCount = 0;
-    P1OUT |= LED_GREEN;
-  } else		          // off for blinkLimit - 1 interrupt periods
-    P1OUT &= ~LED_GREEN;
+  updateGreenBlinking();
+  updateRedBlinking();
 
-  // measure a second
-  secondCount ++;
-  if (secondCount >= 250) {  // once each second
-    secondCount = 0;
-    blinkLimit ++;	     // reduce duty cycle
-    if (blinkLimit >= 8)     // but don't let duty cycle go below 1/7.
-      blinkLimit = 0;
+  secondCount++;
+  if(secondCount>=250*speed){
+    secondCount=0;
+    brightnessPattern();
   }
 } 
+
+void updateGreenBlinking(){
+  greenCount++;
+  if(greenCount >=greenLim){
+    greenCount=0;
+    P1OUT |=LED_GREEN;
+  }else{
+    P1OUT &= ~LED_GREEN;
+  }
+}
+
+void updateRedBlinking(){
+  redCount++;
+  if(redCount >=redLim){
+    redCount=0;
+    P1OUT |=LED_RED;
+  }else{
+    P1OUT &= ~LED_RED;
+  }
+}
+
+void brightnessPattern(){
+  //dim to bright for green
+  greenLim=(state%7)+1;
+
+  //bright to dim for red
+  redLim=8-greenLim;
+
+  state++;
+}
 
