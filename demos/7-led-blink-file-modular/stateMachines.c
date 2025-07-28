ac9e9477657abf9fe601f8b52b89aad2702e1895
“nrmartinez7” <mtnznayeli@gmail.com>
2025-07-27 22:13:09 -0600

    translated to assy, added count feature

--- demos/7-led-blink-file-modular/stateMachines.c
+++ demos/7-led-blink-file-modular/stateMachines.c
@@ -2,47 +2,17 @@
 #include "stateMachines.h"
 #include "led.h"
 
-char toggle_red()		/* always toggle! */
-{
-  static char state = 0;
+static char count=0;
 
-  switch (state) {
-  case 0:
-    red_on = 1;
-    state = 1;
-    break;
-  case 1:
-    red_on = 0;
-    state = 0;
-    break;
-  }
-  return 1;			/* always changes an led */
-}
+void state_advance(){
+  //change red and green based on count
+  green_on=(count>>1)&1;
+  red_on=count&1;
 
-char toggle_green()	/* only toggle green if red is on!  */
-{
-  char changed = 0;
-  if (red_on) {
-    green_on ^= 1;
-    changed = 1;
-  }
-  return changed;
-}
-
-
-void state_advance()		/* alternate between toggling red & green */
-{
-  char changed = 0;  
-
-  static enum {R=0, G=1} color = G;
-  switch (color) {
-  case R: changed = toggle_red(); color = G; break;
-  case G: changed = toggle_green(); color = R; break;
-  }
-
-  led_changed = changed;
+  led_changed=1;
   led_update();
-}
 
+  count=(count+1)%4;
+}
 
 
